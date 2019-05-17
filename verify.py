43d22c1f74c04472f3606e01ea723375e86cb660
nazari <nhn2915@gmail.com>
2019-05-16 22:25:09 -0400

    verifies that collisions will happen

--- verify.py
+++ verify.py
@@ -2,5 +2,31 @@
 # nick nazari
 # may 16 2019
 # this function can be used to verify if a set of values will guarantee corner collisions
+import math
+import yaml
 
-def verify(
+def verify(W,H,w,h,x,y):
+    if abs(x-y) % math.gcd(W - w, H - h) == 0:
+        return True
+
+    return False
+
+if __name__ == "__main__":
+    # loading config values
+    with open('config.yml', 'r') as cfgfile:
+        config = yaml.load(cfgfile, Loader=yaml.FullLoader)
+
+    # window width/height
+    width = config['width']
+    height = config['height']
+    # bouncing box width/height
+    boxwidth = config['boxwidth']
+    boxheight = config['boxheight']
+    # bouncing box starting position
+    x = config['x']
+    y = config['y']
+
+    if verify(boxwidth, boxheight, width, height, x, y):
+        print("This configuration works")
+    else:
+        print("This configuration will not work")
